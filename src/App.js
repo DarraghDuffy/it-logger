6e5c38dda6d3dff3e5a578fcc97e1f9d3fc1e191
darraghduffy <darraghduffy@me.com>
2020-04-17 23:09:59 +0100

    implemented EditLog, Add Tech and Tech list modals

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/tech/AddTechModal';
+import TechListModal from './components/tech/TechListModal';
 import './App.css';
 
 function App() {
@@ -18,7 +21,10 @@ function App() {
       <div className='container'>
         <Logs></Logs>
         <AddBtn></AddBtn>
+        <EditLogModal></EditLogModal>
         <AddLogModal></AddLogModal>
+        <AddTechModal></AddTechModal>
+        <TechListModal></TechListModal>
       </div>
     </Fragment>
   );
