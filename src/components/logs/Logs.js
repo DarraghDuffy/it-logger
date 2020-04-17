85a5f0149ad822e2187afe9db47b31e0d75d3c3a
darraghduffy <darraghduffy@me.com>
2020-04-17 22:32:52 +0100

    log component and log items

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import LogItem from './LogItem';
+import PreLoader from '../layout/PreLoader';
+
+export default function Logs() {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getLogs();
+    //eslint-disable-next-line
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch('/logs');
+    const data = await res.json();
+    setLogs(data);
+    setLoading(false);
+  };
+  if (loading) {
+    return <PreLoader>Loading...</PreLoader>;
+  }
+
+  return (
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className='center'> No logs to show... </p>
+      ) : (
+        logs.map((log) => <LogItem key={log.id} log={log}></LogItem>)
+      )}
+    </ul>
+  );
+}
