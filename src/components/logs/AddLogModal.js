2935517371795af87b27a9f7d99db78f2076a091
darraghduffy <darraghduffy@me.com>
2020-04-17 23:10:31 +0100

    clear some fields out

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -10,6 +10,10 @@ export default function AddLogModal() {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
       console.log(message, tech, attention);
+      //clear fields
+      setMessage('');
+      setTech('');
+      setAttention(false);
     }
   };
   return (
