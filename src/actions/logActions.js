c27c8543cc8aa840a11c1e79f81316dca7eaac97
darraghduffy <darraghduffy@me.com>
2020-04-19 21:07:00 +0100

    implement getLogs and addLog

---
+++ src/actions/logActions.js
@@ -0,0 +1,53 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+
+// export const getLogs = () => {
+//   return async (dispatch) => {
+//     setLoading();
+//     const res = await fetch('/logs');
+//     const data = await res.json();
+//     dispatch({
+//       type: 'GET_LOGS',
+//       payload: data,
+//     });
+//   };
+// };
+
+//this is a refactored approach to above
+//Get Logs from server
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/logs');
+    const data = await res.json();
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({ type: LOGS_ERROR, payload: error.response.data });
+  }
+};
+
+//Set loading to true
+export const setLoading = () => {
+  return { type: SET_LOADING };
+};
+
+//Get Logs from server
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch('/logs', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(log),
+    });
+    const data = await res.json();
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({ type: LOGS_ERROR, payload: error.response.data });
+  }
+};
